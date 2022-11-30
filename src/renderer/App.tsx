ab1e4cec0895180c72b35b1050489381af686e47
Emily LaBelle <emilylabelle8@gmail.com>
2022-11-30 11:34:04 -0500

    built two pages for photo cropper

---
+++ src/renderer/App.tsx
@@ -0,0 +1,16 @@
+import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
+import './App.css';
+import Hello from './component/Hello';
+import PhotoCrop from './component/PhotoCrop';
+
+
+export default function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Hello />} />
+        <Route path="/photo" element={<PhotoCrop />} />
+      </Routes>
+    </Router>
+  );
+}
